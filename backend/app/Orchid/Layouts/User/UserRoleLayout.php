63ae77a9bec0b71b84f14ec8c9cfad1efc8a00db
kotbarsik96 <kotbarsik96@gmail.com>
2024-09-03 10:59:44 +0400

    Еще с ролями

--- backend/app/Orchid/Layouts/User/UserRoleLayout.php
+++ backend/app/Orchid/Layouts/User/UserRoleLayout.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Orchid\Layouts\User;
 
+use App\Models\User;
 use Orchid\Platform\Models\Role;
 use Orchid\Screen\Field;
 use Orchid\Screen\Fields\Select;
@@ -11,19 +12,31 @@ use Orchid\Screen\Layouts\Rows;
 
 class UserRoleLayout extends Rows
 {
-    /**
-     * The screen's layout elements.
-     *
-     * @return Field[]
-     */
-    public function fields(): array
-    {
-        return [
-            Select::make('user.roles.')
-                ->fromModel(Role::class, 'name')
-                ->multiple()
-                ->title(__('Name role'))
-                ->help('Specify which groups this account should belong to'),
-        ];
+  public function getRolesQuery()
+  {
+    $authUserId = auth()->user()->id;
+    $isDeveloper = User::find($authUserId)->inRole('developer');
+
+    if ($isDeveloper) {
+      return Role::where('id', '!=', '0');
+    } else {
+      return Role::whereNotIn('slug', ['developer', 'administrator']);
     }
+  }
+
+  /**
+   * The screen's layout elements.
+   *
+   * @return Field[]
+   */
+  public function fields(): array
+  {
+    return [
+      Select::make('user.roles.')
+        ->fromQuery($this->getRolesQuery(), 'name')
+        ->multiple()
+        ->title(__('Name role'))
+        ->help('Specify which groups this account should belong to'),
+    ];
+  }
 }
