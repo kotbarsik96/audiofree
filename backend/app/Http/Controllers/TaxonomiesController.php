f42655b2fd80420ce318093ed5dcea9614da474f
kotbarsik96 <kotbarsik96@gmail.com>
2024-10-05 10:21:55 +0400

    Каталог

--- backend/app/Http/Controllers/TaxonomiesController.php
+++ backend/app/Http/Controllers/TaxonomiesController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\Taxonomy\Taxonomy;
-use App\Models\Taxonomy\TaxonomyValue;
 
 class TaxonomiesController extends Controller
 {
   public function catalog()
   {
-    $taxonomies = Taxonomy::catalog()->select(['id', 'name', 'slug'])->get();
+    $taxonomies = Taxonomy::catalog()->select(['id', 'name', 'slug'])
+      ->with('values:id,slug,value,value_slug')
+      ->get();
 
     return response([
       'ok' => true,
